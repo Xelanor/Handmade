65b2c1af45fc553bfad2188917bea15eae11131f
Xelanor <berkeozelsel@gmail.com>
2020-08-18 12:28:10 +0300

    Tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        hand: "#042F4B",
+        hand2: "#FFF6DA",
+        hand3: "#FBC99D",
+        hand4: "#ED1250",
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
